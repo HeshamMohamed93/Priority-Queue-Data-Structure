ba15dafa3913673a41a755f56d0705ab0a507521
Hesham <hesham.mohamed19930@gmail.com>
2016-03-07 10:01:41 +0200

    First Commit

---
+++ final2/final/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_totallength = new System.Windows.Forms.Label();
+            this.Current_Time = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "find";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(206, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "show time";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(27, 69);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(242, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(261, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_totallength
+            // 
+            this.lbl_totallength.AutoSize = true;
+            this.lbl_totallength.Location = new System.Drawing.Point(101, 138);
+            this.lbl_totallength.Name = "lbl_totallength";
+            this.lbl_totallength.Size = new System.Drawing.Size(10, 13);
+            this.lbl_totallength.TabIndex = 5;
+            this.lbl_totallength.Text = ":";
+            // 
+            // Current_Time
+            // 
+            this.Current_Time.AutoSize = true;
+            this.Current_Time.Location = new System.Drawing.Point(42, 138);
+            this.Current_Time.Name = "Current_Time";
+            this.Current_Time.Size = new System.Drawing.Size(31, 13);
+            this.Current_Time.TabIndex = 6;
+            this.Current_Time.Text = "0:0:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 262);
+            this.Controls.Add(this.Current_Time);
+            this.Controls.Add(this.lbl_totallength);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_totallength;
+        private System.Windows.Forms.Label Current_Time;
+    }
+}
+
---
+++ final2/final/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Runtime.InteropServices;
+using final;
+using System.IO;
+
+
+
+namespace final
+{
+    public partial class Form1 : Form
+    {
+        int length = 0;
+        OpenFileDialog wav_file = new OpenFileDialog();
+        bool file_existence = false;
+        int timer = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void playsound()
+        {
+            if (file_existence)
+            {
+                SoundPlayer simpleSound = new SoundPlayer(wav_file.FileName); // create a variable for a sound which take the path of the sound
+                simpleSound.Play();
+                // play the sound
+               
+            }
+            else
+            {
+                MessageBox.Show("sorry, you should choose a file.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playsound();
+            timer1.Enabled = true;
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wav_file.Filter = "Wave file (*.wav)|*.wav;"; // specific the selected file only to a wave file extention 
+            if (wav_file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            { // to open a dialog to chosen 
+                //string chosen_file = wav_file.FileName ;
+                //MessageBox.Show(chosen_file);
+                button1.Enabled = true;
+                file_existence = true;
+
+
+                FileStream x = new FileStream(wav_file.FileName, FileMode.Open, FileAccess.Read);  // object have the file and open it reads it's information
+                
+                int leng = (int)x.Length;
+                
+
+                int result = 0;
+                result = ((leng / 1024) * 8) / 1411; // equation about wav file convert length to duration using the bit rate of wav (1141) and length in kbyte
+                length = result;
+               
+               TimeSpan s =  TimeSpan.FromMilliseconds(result*1000);
+                lbl_totallength.Text = s.Hours + ":" + s.Minutes + ":" + s.Seconds;
+                label1.Text =result.ToString()+" "+"sec";
+               trackBar1.Maximum = 100;
+               
+               
+
+
+            }
+        }
+
+ private void button3_Click(object sender, EventArgs e)
+ {
+
+    
+    }
+
+ private void trackBar1_Scroll(object sender, EventArgs e)
+ {
+     
+
+ }
+
+ private void timer1_Tick(object sender, EventArgs e)
+ {
+     TimeSpan ts = TimeSpan.FromMilliseconds(timer);
+     Current_Time.Text = ts.Hours + ":" + ts.Minutes + ":" + ts.Seconds;
+     
+     
+     if (timer < length * 1000)
+     {
+         timer += 10;
+         int value = trackBar1.Maximum * timer / (length * 1000);
+         trackBar1.Value = value;
+     }
+     else
+     {
+         trackBar1.Value = 0;
+         timer1.Stop();
+         ts = TimeSpan.FromMilliseconds(0);
+         timer = 0;
+
+     }
+      
+     
+
+ }
+
+ private void label1_Click(object sender, EventArgs e)
+ {
+
+ }
+
+ private void Form1_Load(object sender, EventArgs e)
+ {
+
+ }
+
+ private void starttime_Tick(object sender, EventArgs e)
+ {
+     
+ }
+
+        }
+    }
+
